a101eea0061577bfc5bae9ccd4633a5313ff57c9
luis.iglesias <luisjavi27@gmail.com>
2022-04-24 15:28:36 -0500

    fix listen

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const res = require('express/lib/response');
 const path = require('path');
 const app = express();
-
+const port=process.env.PORT;
 const publicPath = path.join(__dirname, './public');
 
 app.use(express.static(publicPath));
-app.listen(3000, () => {console.log('Servidor Corriendo (3000)')});
+
+app.listen(port|| 3000, ()=>{
+    console.log('Servidor funcionando en puerto:  ', port);
+    });
+    
+
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'));
 });
