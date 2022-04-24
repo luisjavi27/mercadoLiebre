055b04a162323b41d380cec0a3177934f736efa4
luis.iglesias <luisjavi27@gmail.com>
2022-04-24 15:52:51 -0500

    add register and login html files

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.listen(port|| 3000, ()=>{
-    console.log('Servidor funcionando en puerto:  ', port);
+    console.log('Servidor funcionando en puerto:  ', port||3000);
     });
     
 
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+
+app.get("/login",(req, res) => {
+    res.sendFile(path.join(__dirname,'./views/login.html'));
+});
+
+app.get("/registro",(req, res) => {
+    res.sendFile(path.join(__dirname,'./views/registro.html'));
 });
